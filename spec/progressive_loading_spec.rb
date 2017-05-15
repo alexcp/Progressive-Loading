49ebc6baa26733c7c37655ccbe78abb6463bd65b
Alexandre Croteau-Pothier <alex@alexcp.com>
2017-05-15 15:43:21 -0400

    fix test

--- spec/progressive_loading_spec.rb
+++ spec/progressive_loading_spec.rb
@@ -5,7 +5,25 @@ describe ProgressiveLoading do
     expect(ProgressiveLoading::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe ApplicationHelper, type: :helper do
+    describe "#image_tag" do
+      context "when used normally" do
+        it "returns image tag" do
+          expect(helper.image_tag("image.jpg", class: "test")).to eq("<img class=\"test\" src=\"/assets/image-af8d03c3e231c37e0fcf1985c9df685a557c1feef26002b337e3cb3ef1879b16.jpg\" alt=\"Image\" />")
+        end
+      end
+      context "when passing progressive option" do
+        it "should replace source with provided image" do
+          expect(
+            helper.image_tag("image.jpg", class: "test", progressive: "thumb.jpg")
+          ).to eq("<img class=\"test progressive_loading\" src=\"/images/thumb.jpg\" data-src=\"/assets/image-af8d03c3e231c37e0fcf1985c9df685a557c1feef26002b337e3cb3ef1879b16.jpg\" alt=\"Thumb\" />")
+        end
+        it "should render svg when a color is passed" do
+          expect(
+            helper.image_tag("image.jpg", class: "test", progressive: {color: "#000"})
+          ).to include('src="data:image/svg+xml;base64')
+        end
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require "action_view"
-require "railtie"
-require 'progressive_loading'
+#require "action_view"
+#require 'progressive_loading'
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../dummy/config/environment", __FILE__)
+require "rspec/rails"
